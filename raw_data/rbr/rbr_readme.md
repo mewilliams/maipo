d7dd7a57b2fd7520bc6f1826202f0ef5b3f913f3
mw <megan.williams@usm.cl>
2020-01-03 14:40:14 -0300

    added gitignore, readme on what data should be in RBR Pressure Raw data file

---
+++ raw_data/rbr/rbr_readme.md
@@ -0,0 +1,4 @@
+The files that should be here (on the Google Drive Maipo folder) are:
+
+`042031_20191213_0312.rsk`
+`042031_20191218_1636.rsk`
--- scripts/rbr/quick_plot_rbr_pressure_sensor.m
+++ scripts/rbr/quick_plot_rbr_pressure_sensor.m
@@ -5,6 +5,13 @@
 % plot the pressure data from the Maipo studies.
 % 
 % this script uses RSKtools for reading the RSK files.
-%
+% the toolbox can be found at https://rbr-global.com/support/matlab-tools
+% (though I may copy it to these scripts so they stand alone...)
+% 
+% version: 3 jan 2020: just plot the data.
+% 
+% any updates please comment.
+
+load ../raw_data/rbr/
 
 
