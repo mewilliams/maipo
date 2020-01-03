bee7f135822815b19c3f0dbe701d61235b92842e
mw <megan.williams@usm.cl>
2020-01-03 14:26:06 -0300

    first commit

---
+++ scripts/rbr/quick_plot_rbr_pressure_sensor.m
@@ -0,0 +1,10 @@
+% quick_plot_rbr_pressure_sensor.m 
+% 03 January 2020
+% m. williams
+%
+% plot the pressure data from the Maipo studies.
+% 
+% this script uses RSKtools for reading the RSK files.
+%
+
+
