3e150a7c46611ffa6356af5f1aa7e8fa4beaef7a
mw <megan.williams@usm.cl>
2020-01-16 16:06:18 -0300

    updated with ensemble numbers

--- scripts/adcp/adcp_01_readfiles_maipo.m
+++ scripts/adcp/adcp_01_readfiles_maipo.m
@@ -1,5 +1,9 @@
 % 16 january 2020
 % M. Williams
+% this file reads the two raw datafiles and outputs them to matlab format.
+% uses rdradcp.m
+% saves to edited_data folder
+% 
 
 clear;
 close all
@@ -16,9 +20,9 @@ saveloc = '../../edited_data/adcp/'
 %  [..]=rdradcp(NAME,NUMAV,[NFIRST NEND])
 [adcp,cfg]=rdradcp(filenameday1,1,[1 57557]); % last ensemble 57558, but sends a zero time, so eliminate
 datasource = ['data from ',filenameday1];
-save([saveloc,'adcp_day1_december_2019_maipo'],adcp,cfg,datasource)
+save([saveloc,'adcp_day1_december_2019_maipo'],'adcp','cfg','datasource')
 clearvars -except filenameday* saveloc
 
 datasource = ['data from ',filenameday2];
-[adcp,cfg]=rdradcp(filenameday2,1,-1);
-save([saveloc,'adcp_day2_december_2019_maipo'],adcp,cfg,filenameday2)
+[adcp,cfg]=rdradcp(filenameday2,1,[1 50079]); % last ensemble 50080, but sends a zero time, so eliminate
+save([saveloc,'adcp_day2_december_2019_maipo'],'adcp','cfg','datasource')
