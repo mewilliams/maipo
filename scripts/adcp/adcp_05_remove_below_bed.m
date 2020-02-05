1e81f31b16790b37b6a69f5047bedd5dfd0b9f54
Megan Williams <megams@noc.ac.uk>
2020-02-05 15:47:24 -0300

    usable dataset

--- scripts/adcp/adcp_05_remove_below_bed.m
+++ scripts/adcp/adcp_05_remove_below_bed.m
@@ -2,9 +2,26 @@ clear;
 close all;
 
 load ../../edited_data/adcp/in_water_indices_rdi_adcp.mat
+
+% day 2
 load ../../edited_data/adcp/adcp_day2_december_2019_maipo.mat
 load ../../edited_data/adcp/lastbinindex_day2.mat
+savename = ['../../edited_data/adcp/adcp_day2_december_2019_maipo_usable.mat'];
+startidx = startidx_day2;
+endidx = endidx_day2;
+runtwice(adcp,cfg,lastbinidx,startidx,endidx,savename)
 
+
+% day 1
+load ../../edited_data/adcp/adcp_day1_december_2019_maipo.mat
+load ../../edited_data/adcp/lastbinindex_day1.mat
+savename = ['../../edited_data/adcp/adcp_day1_december_2019_maipo_usable.mat'];
+startidx = startidx_day1;
+endidx = endidx_day1;
+runtwice(adcp,cfg,lastbinidx,startidx,endidx,savename)
+
+
+function [] = runtwice(adcp,cfg,lastbinidx,startidx,endidx,savename)
 eastvel = adcp.east_vel;
 subplot(3,1,1)
 imagesc(adcp.mtime,cfg.ranges,eastvel), shading flat, title('raw')
@@ -33,6 +50,16 @@ end
 imagesc(adcp.mtime,cfg.ranges,eastvel), shading flat, title('edited + 1')
 
 
+eastvel(:,1:startidx) = NaN;
+eastvel(:,endidx:end) = NaN;
+northvel(:,1:startidx) = NaN;
+northvel(:,endidx:end) = NaN;
+vertvel(:,1:startidx) = NaN;
+vertvel(:,endidx:end) = NaN;
+
+save(savename,'eastvel','northvel','vertvel')
+
+
 figure
 subplot(311)
 imagesc(adcp.mtime,cfg.ranges,eastvel), shading flat, title('edited + 1')
@@ -43,33 +70,4 @@ imagesc(adcp.mtime,cfg.ranges,northvel), shading flat, title('edited + 1')
 subplot(313)
 imagesc(adcp.mtime,cfg.ranges,vertvel), shading flat, title('edited + 1')
 
-
-%%
-k = ones(5,1);
-k = k./sum(k);
-sum(k)
-evconv = NaN(size(eastvel));
-nvconv = NaN(size(eastvel));
-vvconv = NaN(size(eastvel));
-for j = 1:19
-evconv(j,:) = conv(eastvel(j,:),k,'same');
-nvconv(j,:) = conv(northvel(j,:),k,'same');
-vvconv(j,:) = conv(vertvel(j,:),k,'same');
 end
-
-figure
-subplot(311)
-pcolor(adcp.mtime,cfg.ranges,evconv), shading flat, title('conv east'), colorbar
-
-subplot(312)
-pcolor(adcp.mtime,cfg.ranges,nvconv), shading flat,title('conv north'), colorbar
-
-subplot(313)
-pcolor(adcp.mtime,cfg.ranges,vvconv), shading flat, title('conv vert'), colorbar
-%% 
-
-figure
-plot(adcp.mtime,eastvel(1,:)), hold all
-plot(adcp.mtime,conv(eastvel(1,:),k,'same'))
-plot(adcp.mtime,evconv(1,:))
-% plot(adcp.mtime,evconv(1,:))
