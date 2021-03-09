20b2dfc8f3f79c6d747bfed14d4e67f9294bdbe7
Megan Williams <megams@noc.ac.uk>
2021-03-09 12:27:19 -0300

    save draft of fig 2 for grl paper as fig

--- scripts/ig_paper_image_scripts/fig2_ig_inlet.m
+++ scripts/ig_paper_image_scripts/fig2_ig_inlet.m
@@ -12,12 +12,14 @@ jgr_figure_startup % sets axes out, small fonts.
 clear
 close all;
 
-% run wavelet script (it also plots - btu the plotting of wavelet and
+% run wavelet script (it also plots - but the plotting of wavelet and
 % pressure just copied from over there once variables are made)
 returnhere = pwd;
 cd ../rbr/
 run('quick_wavelet_maipo_pressure.m')
 cd(returnhere)
+
+xl = [datenum(2019,12,10,18,0,0) datenum(2019,12,12,18,0,0)];
 
 
 %this file generated in adcp_07_ig_inlet_figure.m:
@@ -32,9 +34,11 @@ figure
 ax(3) = subplot(6,1,4)
 plot(timefill,U_fill10ma), hold all, grid on
 plot(timefill,U_6minsmooth)
+xlim(xl)
 
 ax(4) = subplot(6,1,5)
 plot(timefill,sqrt(movvar(U_fill10ma-U_6minsmooth,600))), hold all, grid on
+xlim(xl)
 
 % return;
 
@@ -46,15 +50,19 @@ ax(3) = subplot(6,1,4)
 plot(timefill,U_fill10ma)
 plot(timefill,U_6minsmooth)
 ylabel('velocity [m/s]')
+xlim(xl)
 
 ax(4) = subplot(6,1,5)
 plot(timefill,sqrt(movvar(U_fill10ma-U_6minsmooth,600))), hold all, grid on
 ylabel('IG vel. [m/s]')
+xlim(xl)
 
 
 ax(1) = subplot(6,1,1:2)
 pcolor(t1(idx),f1,abs(wt1)), shading flat
 ylabel({'frequency','[hz]'})
+xlim(xl)
+
 % colorbar
 disp(caxis)
 caxis(ca)
@@ -71,11 +79,11 @@ fillT(end+1) = fillT(end);
 
 fillcoi = coi;
 fillcoi(2:end+1) = fillcoi;
-fillcoi(1) = min(coi); % not zero because axis is log
+fillcoi(1) = min(coi); % not ze'ro because axis is log
 fillcoi(end+1) = min(coi);
 fill(fillT,fillcoi,'k')
 
-datetick('x')
+datetick('x','keeplimits')
 
 ax(2) = subplot(6,1,3);
 plot(t1,p1), grid on
@@ -85,13 +93,23 @@ p1_6min = smooth(p1,6*60*16);
 plot(t1,p1_6min)
 ylabel({'pressure','[dbar]'})
 
-datetick('x')
+% datetick('x')'
 
 ax(5) = subplot(6,1,6);
 % plot(t1,movvar(p1-p1_6min,6*60*16).^2)
 plot(t1,movstd(p1-p1_6min,6*60*16)), hold all
 grid on
 ylabel('IG pres. [dbar]')
+xlabel('10 - 12 December 2019')
 % colorbar
 
 % cd 
+
+
+for i = 3:6
+    subplot(6,1,i)
+    xlim(xl)
+    datetick('x','keeplimits')
+end
+
+savefig(gcf,'../../images/fig2_draft.fig')
